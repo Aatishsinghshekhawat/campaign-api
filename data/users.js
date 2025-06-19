33639e8add98195ee0da4928a07e58e113b24bf7
Aatishsinghshekhawat <aatishsinghdbs@gmail.com>
2025-06-19 20:58:01 +0530

    modified structure

--- data/users.js
+++ data/users.js
@@ -1,3 +1,2 @@
 let users = [];
-
 module.exports = users;
--- database.sql
+++ database.sql
@@ -2,6 +2,18 @@ CREATE DATABASE origin;
 
 USE origin;
 
+CREATE TABLE `user` (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100) UNIQUE,
+    mobileCountryCode INT,
+    password VARCHAR(255),
+    role_id INT,
+    createdDate DATETIME,
+    modifiedDate DATETIME,
+    FOREIGN KEY(role_id) REFERENCES role(id)
+);
+
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100),
@@ -9,13 +21,24 @@ CREATE TABLE role (
     modifiedDate DATETIME 
 );
 
-CREATE TABLE user (
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(50),
-mobileCountryCode INT,
-password VARCHAR(50),
-role_id INT,
-createdDate DATETIME,
-modifiedDate DATETIME,
-FOREIGN KEY(role_id)  REFERENCES role(id)
- );
+
+
+CREATE TABLE list (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name CHAR(25),
+    createdDate DATETIME,
+    modifiedDate DATETIME
+);
+
+CREATE TABLE list_item (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    list_id INT,
+    email VARCHAR(50),
+    variables JSON,
+    createdDate DATETIME,
+    modifiedDate DATETIME,
+    FOREIGN KEY (list_id) REFERENCES list(id)
+);
+
+SELECT * FROM user WHERE email = 'aatish@gmail.com';
+
