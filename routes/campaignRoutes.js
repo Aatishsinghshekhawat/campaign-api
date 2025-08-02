e4506e00ec127497ba48e5eb6fcd9ea2caacfc0a
Aatishsinghshekhawat <aatishsinghdbs@gmail.com>
2025-08-02 23:42:58 +0530

    updated routes

--- routes/campaignRoutes.js
+++ routes/campaignRoutes.js
@@ -1,1 +1,20 @@
-campaignRoutes.js
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/campaignController');
+const authenticateToken = require('../middleware/authMiddleware');
+const { createCampaignValidator, updateCampaignValidator } = require('../validators/campaignValidator');
+const validateRequest = require('../middleware/validateRequest');
+
+router.post('/create', authenticateToken, createCampaignValidator, validateRequest, controller.createCampaign);
+
+router.post('/list', authenticateToken, controller.getCampaigns);
+
+router.get('/:id', authenticateToken, controller.getCampaignById);
+
+router.put('/update/:id', authenticateToken, updateCampaignValidator, validateRequest, controller.updateCampaign);
+
+router.delete('/delete/:id', authenticateToken, controller.deleteCampaign);
+
+router.post('/copy/:id', authenticateToken, controller.copyCampaign);
+
+module.exports = router;
